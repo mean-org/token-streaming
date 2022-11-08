6e342b470e6fca50014d14413438b07f10df0bcd
eydelrivero <eydel.rivero@gmail.com>
2022-11-08 19:28:30 +0100

    Fixed program deploy script

--- deploy.devnet.sh
+++ deploy.devnet.sh
@@ -14,6 +14,11 @@ then
       echo "Please provide correct environment or RPC url"
       exit 3
 fi
+if [ -z "$MINIMUM_SOL_NEEDED" ]
+then
+      echo "Please provide correct minimum SOL needed for deployment"
+      exit 4
+fi
 
 # solana cli overwrite env
 solana config set --url $RPC_URL
@@ -25,25 +30,33 @@ SOL_BALANCE="$(solana balance)"
 echo "SOL balance: $SOL_BALANCE"
 
 # Get balance amount & compare
-SOL_BALANCE_AMOUNT=$(echo $SOL_BALANCE | grep -Po '\d+' | head -1 | grep -Po '\d+')
+SOL_BALANCE_AMOUNT=$(echo $SOL_BALANCE | sed -E  's/(.*) (.*)/\1/')
+echo "SOL_BALANCE_AMOUNT: $SOL_BALANCE_AMOUNT"
 MAX_AIRDROP_ATTEMPS=3
 AIRDROP_ATTEMPS=$MAX_AIRDROP_ATTEMPS
+SLEEP_SECONDS=2
 
-while [ "$AIRDROP_ATTEMPS" -ge 0 && "$SOL_BALANCE_AMOUNT" -le "$MINIMUM_SOL_NEEDED" ]
+while (( $(echo "$AIRDROP_ATTEMPS > 0" | bc -l) )) && (( $(echo "$SOL_BALANCE_AMOUNT <= $MINIMUM_SOL_NEEDED" | bc -l) ))
 do
       echo "SOL balance is LOW. At least $MINIMUM_SOL_NEEDED SOL are needed. The wallet has $SOL_BALANCE"
       echo "Requesting SOL..."
-      if [ "$AIRDROP_ATTEMPS" -lt "$MAX_AIRDROP_ATTEMPS" ]; then sleep 2; fi
+      if [ "$AIRDROP_ATTEMPS" -lt "$MAX_AIRDROP_ATTEMPS" ]; 
+      then 
+            echo "Sleeping for $SLEEP_SECONDS ..."
+            sleep $SLEEP_SECONDS; 
+      fi
       solana airdrop 2
       AIRDROP_ATTEMPS=$(( $AIRDROP_ATTEMPS - 1 ))
+      echo "AIRDROP_ATTEMPS: $AIRDROP_ATTEMPS"
       SOL_BALANCE="$(solana balance)"
-      SOL_BALANCE_AMOUNT=$(echo $SOL_BALANCE | grep -Po '\d+' | head -1 | grep -Po '\d+')
+      SOL_BALANCE_AMOUNT=$(echo $SOL_BALANCE | sed -E  's/(.*) (.*)/\1/')
+      let SLEEP_SECONDS=2*$SLEEP_SECONDS
 done
 
-if [ "$SOL_BALANCE_AMOUNT" -le "$MINIMUM_SOL_NEEDED" ]
+if (( $(echo "$SOL_BALANCE_AMOUNT <= $MINIMUM_SOL_NEEDED" | bc -l) ))
 then
       echo "SOL balance is LOW. At least $MINIMUM_SOL_NEEDED SOL are needed. The wallet has $SOL_BALANCE. Aborting after $MAX_AIRDROP_ATTEMPS airdrop attemps..."
-      exit 4
+      exit 5
 fi
 
 # anchor cli
@@ -56,7 +69,7 @@ echo "{BUFFER_ACCOUNT_ADDRESS}={$BUFFER_ACCOUNT_ADDRESS}" >> $GITHUB_ENV
 if [ -z "$BUFFER_ACCOUNT_ADDRESS" ]
 then
       echo "Deploy failed..."
-      exit 5
+      exit 6
 else
       echo "Updating buffer authority..."
       solana program set-buffer-authority $BUFFER_ACCOUNT_ADDRESS --new-buffer-authority $MULTISIG_AUTHORITY_ADDRESS
