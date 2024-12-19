0de146df47027d363825e524fd58898674156069
Yamel Amador Fernandez <yamel_amador_fdez@msn.com>
2024-12-19 17:32:29 -0600

    Try alternative keypair loding mechanism

--- sdks/payment-streaming/tests/utils.ts
+++ sdks/payment-streaming/tests/utils.ts
@@ -1,9 +1,8 @@
-import * as fs from "fs-extra";
-import { join } from "path";
-import { homedir } from "os";
 import { Keypair, Transaction } from '@solana/web3.js';
-import BN from "bn.js";
 import BigNumber from "bignumber.js";
+import BN from "bn.js";
+import base58 from 'bs58';
+import * as fs from "fs-extra";
 
 export const getDefaultKeyPair = async (): Promise<Keypair> => {
     // const id = await fs.readJSON(join(homedir(), '.config/solana/id.json'));
@@ -11,6 +10,31 @@ export const getDefaultKeyPair = async (): Promise<Keypair> => {
     // return Keypair.fromSecretKey(bytes);
 
     return Keypair.generate();
+};
+
+export const getKeypairFromJson = (filePath: string) => {
+    const secretKeyString = fs.readFileSync(filePath, { encoding: 'utf-8' });
+
+    // Try the shorter base58 format first
+    let decodedSecretKey: Uint8Array;
+    try {
+        decodedSecretKey = base58.decode(secretKeyString);
+        return Keypair.fromSecretKey(decodedSecretKey);
+    } catch (throwObject) {
+        const error = throwObject as Error;
+        if (!error.message.includes('Non-base58 character')) {
+            throw new Error('Invalid secret key provided!');
+        }
+    }
+
+    // Try the longer JSON format
+    try {
+        decodedSecretKey = Uint8Array.from(JSON.parse(secretKeyString));
+    } catch (error) {
+        throw new Error('Invalid secret key provided!');
+    }
+
+    return Keypair.fromSecretKey(decodedSecretKey);
 };
 
 export const _printSerializedTx = (tx: Transaction, requireAllSignatures = false, verifySignatures = false) => {
@@ -33,10 +57,10 @@ export const toTokenAmountBn = (amount: number | string, decimals: number) => {
     // if (!amount || !decimals) {
     //   return new BN(0);
     // }
-  
+
     const multiplier = new BigNumber(10 ** decimals);
     const value = new BigNumber(amount);
     const result = value.multipliedBy(multiplier).integerValue();
     const toFixed = result.toFixed(0);
     return new BN(toFixed);
-  }
+}
